fdae15335d1cd94a997167155aa1aa5cf9efcfb3
Sunny Hirai <thesunny@gmail.com>
2022-04-26 16:23:50 -0700

    Add record support to debug

--- src/debug/index.ts
+++ src/debug/index.ts
@@ -1,8 +1,15 @@
 import chalk from "chalk"
 import jsome from "jsome"
 
-// let logIfEnabled = console.log
+let buffer: string[] = []
+
 let logEnabled = true
+let recordEnabled = false
+
+function log(s: string) {
+  if (logEnabled) console.log(s)
+  if (recordEnabled) buffer.push(s)
+}
 
 export const debug = {
   enable() {
@@ -11,34 +18,46 @@ export const debug = {
   disable() {
     logEnabled = false
   },
+  record() {
+    recordEnabled = true
+  },
+  stop() {
+    recordEnabled = false
+  },
+  play(): string[] {
+    const oldBuffer = buffer
+    buffer = []
+    return oldBuffer
+  },
   request(id: number, props: unknown) {
-    if (!logEnabled) return
-    console.log(chalk.hex("32cd32")(`== Request (${id}) ==`))
-    if (props) {
-      jsome(props)
-    }
+    const lines: string[] = []
+    lines.push(chalk.hex("32cd32")(`== Request (${id}) ==`))
+    lines.push(jsome.getColoredString(props))
+    log(lines.join("\n"))
   },
   response(id: number, diff: number, response: unknown) {
-    if (!logEnabled) return
-    console.log(chalk.hex("32cd32")(`== Response (${id}) ${diff}ms ==`))
-    jsome(response)
+    const lines: string[] = []
+    lines.push(chalk.hex("32cd32")(`== Response (${id}) ${diff}ms ==`))
+    lines.push(jsome.getColoredString(response))
+    log(lines.join("\n"))
   },
   error(id: number, error?: Error) {
-    if (!logEnabled) return
-    console.log(chalk.hex("b22222")(`== Error (${id}) ==`))
-    if (error === undefined) return
-    if (error.stack == null) return
-    error.stack.split("\n").forEach((line: string) => {
-      /**
-       * Dim stack lines that come from `node_modules` because they are less
-       * important. We want the lines from our source code to be more
-       * prominent.
-       */
-      if (line.includes("/node_modules/")) {
-        console.log(chalk.hex("800000")(line))
-      } else {
-        console.log(chalk.red(line))
-      }
-    })
+    const lines: string[] = []
+    lines.push(chalk.hex("b22222")(`== Error (${id}) ==`))
+    if (error && error.stack != null) {
+      error.stack.split("\n").forEach((line: string) => {
+        /**
+         * Dim stack lines that come from `node_modules` because they are less
+         * important. We want the lines from our source code to be more
+         * prominent.
+         */
+        if (line.includes("/node_modules/")) {
+          lines.push(chalk.hex("800000")(line))
+        } else {
+          lines.push(chalk.red(line))
+        }
+      })
+    }
+    log(lines.join("\n"))
   },
 }
--- types/jsome/index.d.ts
+++ types/jsome/index.d.ts
@@ -1,3 +1,15 @@
 declare module "jsome" {
-  export default function jsome(value: unknown): void
+  const jsome: {
+    (value: unknown): void
+    getColoredString: (value: unknown) => string
+  }
+  export default jsome
+  // export default {
+  //   getColoredString: (value: unknown) => void
+  // } & (function jsome(value: unknown): void)
+  // export default function jsome(value: unknown): void
+  // type JSome = (value: unknown) => void
+  // type getColoredString = (value: unknown) => void
+  // type Merged = Jsome & { getColoredString }
+  // export default Merged
 }
