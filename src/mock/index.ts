1b86e7d2bef99878c57c7a78842116712858879c
Sunny Hirai <thesunny@gmail.com>
2022-05-03 14:41:02 -0700

    Export Client, Mock and Web as subpaths

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,14 +1,3 @@
-import { GetServerSideProps, GetServerSidePropsContext } from "next"
-import { ParsedUrlQuery } from "querystring"
-
-export * from "./call"
-
-export async function callSideHandler<
-  Q extends ParsedUrlQuery,
-  F extends GetServerSideProps
->(query: Q, fn: F) {
-  const mockContext = { query } as unknown as GetServerSidePropsContext<Q>
-  const result = await fn(mockContext)
-  const props = "props" in result ? result.props : undefined
-  return [props, result]
-}
+import * as Mock from "./_index"
+export default Mock
+export * from "./_index"
--- src/web/index.ts
+++ src/web/index.ts
@@ -1,5 +1,3 @@
-export * from "./get-server-side-props"
-export * from "./get-static-props"
-export * from "./get-static-paths"
-export * from "./page"
-export * from "./web-response"
+import * as Web from "./_index"
+export default Web
+export * from "./_index"
